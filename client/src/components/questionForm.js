ec3f6109940d6308c5abd8b18fcbf4844fc70ac6
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-11-10 00:04:24 +0900

    대분류 질문 퍼블리싱

---
+++ client/src/components/questionForm.js
@@ -0,0 +1,172 @@
+import React from "react";
+import styled from 'styled-components';
+import traffic from '../assets/icons/traffic_light.png';
+import check from '../assets/icons/check.png';
+
+
+function QuestionForm({question, name}){
+    return(
+        <StyledQuestionForm>
+
+        <form id="category" action="http://127.0.0.1:8000/api/v1/category" method="post">
+        
+        <fieldset>
+          <legend><img src={traffic} alt=""/>{question}</legend>
+          <div className='radiowrap'>
+            <label className='first'>
+              <input className='first' type="radio" name={name} value="-2" />
+              비동의<span></span>
+            </label>
+            <label>
+              <input className='second' type="radio" name={name} value="-1" />
+              <span></span>
+            </label>
+            <label>
+              <input className='third' type="radio" name={name} value="0" />
+              <span></span>
+            </label>
+            <label>
+              <input className='fourth' type="radio" name={name} value="1" />
+              <span></span>
+            </label>
+            <label className='fifth'>
+              <input className='fifth' type="radio" name={name} value="2" />
+              <span></span>동의
+            </label>
+            
+          </div>
+        </fieldset>
+    </form>
+   </StyledQuestionForm>
+    )
+}
+
+export default QuestionForm;
+
+const StyledQuestionForm = styled.div`
+     & form{
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  & legend{
+    position: absolute;
+    width: 27rem;
+    height: 13rem;
+    left: 50%;
+    transform: translate(-50%, 50%);
+    top: -5rem;
+    line-height: 13rem;
+    background: #FFFFFF;
+    box-shadow: 0px 3px 5px rgba(152, 152, 152, 0.24);
+    border-radius: 15px;
+    text-align: center;
+    color: ${(props) => props.theme.colors.Engblue3};
+    font-size: 1.5rem;
+    font-family: 'Pretendard-SemiBold';
+  }
+  
+  & legend img{
+    position: absolute;
+    width: 29px;
+    height: 7px;
+    top: 1.4rem;
+    left: 2rem;
+
+  }
+  & .radiowrap{
+    position: absolute;
+    top: 15.5rem;
+    left:-1rem;
+    transform: translate(-50%, 50%);
+    width: 30rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    font-family: 'Pretendard-SemiBold';
+  }
+  & .radiowrap label.first{
+    color: ${(props) => props.theme.colors.Engpink};
+    font-size: 1.5rem;
+  }
+  & .radiowrap label.fifth{
+    color: ${(props) => props.theme.colors.Enggreen};
+    font-size: 1.5rem;
+  }
+
+  & input[type="radio"]{
+    appearance: none;
+  }
+
+  & input[type="radio"] + span{
+    display: inline-block;
+    width: 37px;
+    height: 37px;
+    border: 2px solid ${(props) => props.theme.colors.Engpink};
+    border-radius: 50%;
+    cursor: pointer;
+    vertical-align: middle;
+  }
+  & input[type="radio"].first + span{
+    margin-left: 10px;
+  }
+  & input[type="radio"].first:checked + span{
+    background-color: ${(props) => props.theme.colors.Engpink};
+    background-image: url(${check});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 18px 18px;
+  }
+  & input[type="radio"].second + span{
+    width: 27px;
+    height: 27px;
+  }
+  & input[type="radio"].second:checked + span{
+    background-color: ${(props) => props.theme.colors.Engpink};
+    background-image: url(${check});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 16px 16px;
+  }
+  & input[type="radio"].third + span{
+    width: 22px;
+    height: 22px;
+    border-color: ${(props) => props.theme.colors.Enggray2};
+  }
+  & input[type="radio"].third:checked + span{
+    background-color: ${(props) => props.theme.colors.Enggray2};
+    background-image: url(${check});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 14px 14px;
+  }
+  & input[type="radio"].fourth + span{
+    width: 27px;
+    height: 27px;
+    border-color: ${(props) => props.theme.colors.Enggreen};
+  }
+  & input[type="radio"].fourth:checked + span{
+    background-color: ${(props) => props.theme.colors.Enggreen};
+    background-image: url(${check});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 16px 16px;
+  }
+
+  & input[type="radio"].fifth + span{
+    width: 37px;
+    height: 37px;
+    border-color: ${(props) => props.theme.colors.Enggreen};
+    margin-right: 10px;
+  }
+  & input[type="radio"].fifth:checked + span{
+    background-color: ${(props) => props.theme.colors.Enggreen};
+    background-image: url(${check});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 18px 18px;
+  }
+
+
+`;
--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -1,97 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import arrow from '../assets/icons/arrow_right.png';
+import QuestionForm from '../components/questionForm';
+
 
 function Test() {
+  const [num, setnum] = useState(1);
+  const incrementNum = () => {
+    setnum(prev => prev + 1 );
+  };
+  
     return(
-        <div>
-      <form action="http://127.0.0.1:8000/api/v1/category" method="post">
-
-        <fieldset>
-          <legend>질문1</legend>
-          <input type="radio" name="q1" value="-2" />
-          전혀 그렇지 않다
-          <input type="radio" name="q1" value="-1" />
-          그렇지 않다
-          <input type="radio" name="q1" value="0" />
-          보통이다
-          <input type="radio" name="q1" value="1" />
-          그렇다
-          <input type="radio" name="q1" value="2" />
-          매우 그렇다
-        </fieldset>
-
-        <fieldset>
-          <legend>질문2</legend>
-          <input type="radio" name="q2" value="-2" />
-          전혀 그렇지 않다
-          <input type="radio" name="q2" value="-1" />
-          그렇지 않다
-          <input type="radio" name="q2" value="0" />
-          보통이다
-          <input type="radio" name="q2" value="1" />
-          그렇다
-          <input type="radio" name="q2" value="2" />
-          매우 그렇다
-        </fieldset>
-
-        <fieldset>
-          <legend>질문3</legend>
-          <input type="radio" name="q3" value="-2" />
-          전혀 그렇지 않다
-          <input type="radio" name="q3" value="-1" />
-          그렇지 않다
-          <input type="radio" name="q3" value="0" />
-          보통이다
-          <input type="radio" name="q3" value="1" />
-          그렇다
-          <input type="radio" name="q3" value="2" />
-          매우 그렇다
-        </fieldset>
-
-        <fieldset>
-          <legend>질문4</legend>
-          <input type="radio" name="q4" value="-2" />
-          전혀 그렇지 않다
-          <input type="radio" name="q4" value="-1" />
-          그렇지 않다
-          <input type="radio" name="q4" value="0" />
-          보통이다
-          <input type="radio" name="q4" value="1" />
-          그렇다
-          <input type="radio" name="q4" value="2" />
-          매우 그렇다
-        </fieldset>
-
-        <fieldset>
-          <legend>질문5</legend>
-          <input type="radio" name="q5" value="-2" />
-          전혀 그렇지 않다
-          <input type="radio" name="q5" value="-1" />
-          그렇지 않다
-          <input type="radio" name="q5" value="0" />
-          보통이다
-          <input type="radio" name="q5" value="1" />
-          그렇다
-          <input type="radio" name="q5" value="2" />
-          매우 그렇다
-        </fieldset>
-
-        <fieldset>
-          <legend>질문6</legend>
-          <input type="radio" name="q6" value="-2" />
-          전혀 그렇지 않다
-          <input type="radio" name="q6" value="-1" />
-          그렇지 않다
-          <input type="radio" name="q6" value="0" />
-          보통이다
-          <input type="radio" name="q6" value="1" />
-          그렇다
-          <input type="radio" name="q6" value="2" />
-          매우 그렇다
-        </fieldset>
-        <button type="submit">제출하기</button>
-      </form>
-    </div>
+        <StyledTest>
+        <h2>왓욜넴</h2>
+        
+        <div className={num === 1 ? "topQuestion" : "topQuestion inactive"}>
+          <QuestionForm question='질문1' name='q1' />
+        </div>
+        <div className={num === 1 ? "bottomQuestion" : "bottomQuestion inactive"}>
+          <QuestionForm question='질문2' name='q2'/>
+        </div>
+        <div className={num === 2 ? "topQuestion" : "topQuestion inactive"}>
+          <QuestionForm question='질문3' name='q3' />
+        </div>
+        <div className={num === 2 ? "bottomQuestion" : "bottomQuestion inactive"}>
+          <QuestionForm question='질문4' name='q4'/>
+        </div>
+        <div className={num === 3 ? "topQuestion" : "topQuestion inactive"}>
+          <QuestionForm question='질문5' name='q5' />
+        </div>
+        <div className={num === 3 ? "bottomQuestion" : "bottomQuestion inactive"}>
+          <QuestionForm question='질문6' name='q6'/>
+        </div>
+        <button type={num === 4 ? "submit" : "button"} onClick={incrementNum} form='category'>
+          <span>다음으로</span>
+          <img src={arrow} alt="" />
+        </button>
+      </StyledTest>
     );
 }
 
 export default Test;
+
+const StyledTest = styled.div`
+  h2 {
+    font-family: 'Pretendard-ExtraBold';
+    color: ${(props) => props.theme.colors.Engblue3};
+    font-size: 1.5rem;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    top: 4rem;
+    text-align: center;
+    }
+
+& .inactive{
+  display: none;
+}
+& .topQuestion {
+  position: absolute;
+  top: 12vh;
+}
+
+  & .bottomQuestion {
+    position: absolute;
+    top: 48vh;
+  }
+
+  & button[type="button"] {
+        position: absolute;
+        width: 16.8rem;
+        height: 10rem;
+        max-width: 303px;
+        max-height: 60px;
+        left: 50%;
+        top: 93vh;
+        transform: translate(-50%, -50%);
+
+        background-color: ${(props) => props.theme.colors.Engblue1};
+        box-shadow: 0px 3px 8px 2px rgba(166, 221, 255, 0.63);
+        border-radius: 30px;
+    }
+
+& button[type="button"] span {
+    color: ${(props) => props.theme.colors.Engwhite};
+    font-size: 1.5rem;
+    font-family: 'Pretendard-SemiBold';
+    
+}
+
+
+& button[type="button"] img {
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    left: 82%;
+}
+
+& button[type="submit"] {
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+
+`;
