7595f8de750404daf43b3c8c9d8238caa95f49e8
HGB <hsw8147@gmail.com>
2022-11-10 22:12:33 +0900

    data trans problem

--- client/src/components/questionForm.js
+++ client/src/components/questionForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import traffic from "../assets/icons/traffic_light.png";
 import check from "../assets/icons/check.png";
