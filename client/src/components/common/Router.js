16abfbbdec6fff2a61b7f260e716d8f94d64212b
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-11-10 21:09:50 +0900

    소분류 테스트 라우팅

--- client/src/components/common/Router.js
+++ client/src/components/common/Router.js
@@ -3,6 +3,7 @@ import Test from '../../pages/Test';
 import Result from '../../pages/Result';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import DetailTest from '../../pages/DetailTest';
 
 function Router() {
     return (
@@ -10,6 +11,7 @@ function Router() {
             <Routes>
                 <Route path='/' element={ <Main/> }/>
                 <Route path='/Test' element={ <Test/> }/>
+                <Route path='/Detail/:id' element={ <DetailTest/> }/>
                 <Route path='/Result/:id' element={ <Result/> }/>
             </Routes>
         </BrowserRouter>
